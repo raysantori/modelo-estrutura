3affe2e39e2382c57f6c75597bc1d0418e301346
raysantori <rayssacgomess@gmail.com>
2022-06-28 22:46:17 -0300

    feat. favicon + footer

---
+++ assets/scripts/script.js
@@ -0,0 +1,5 @@
+// footer copyright
+var copyright = window.document.querySelector('#copyright')
+var dataAtual = new Date()
+var anoAtual = dataAtual.getFullYear()
+copyright.innerHTML = `&copy; ${anoAtual} RaySantori`
